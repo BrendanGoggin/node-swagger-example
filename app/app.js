17a0b65a75e922a5abe42c5ef3035f73e348c423
Brendan Goggin <bjg3wk@virginia.edu>
2019-02-18 01:10:07 -0500

    change users to user

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const userRouter = require('./routes/user');
 
 const app = express();
 
@@ -15,6 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', userRouter);
 
 module.exports = app;
